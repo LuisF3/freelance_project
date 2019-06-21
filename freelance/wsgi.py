c9e40e03772ce76d14d2f534ce6cced690725013
Luís Felipe Ribeiro Chaves <pi.luis.f3@gmail.com>
2019-06-20 21:38:15 -0300

    dj-static filter in wsgi.py for media files

--- freelance/wsgi.py
+++ freelance/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
+from dj_static import Cling, MediaCling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freelance.settings')
 
-application = Cling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application()))
